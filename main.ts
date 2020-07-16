44df68275784e846fb0b2b6d13bf6dcf94598d2d
ihzi999 <68045809+ihzi999@users.noreply.github.com>
2020-07-16 14:50:59 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(led2, 0)
+    led2 = Math.min(4, led2 - 1)
+    led.plot(led2, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(led2, 0)
+    led2 = Math.min(4, led2 + 1)
+    led.plot(led2, 0)
+})
+let led2 = 0
+led2 = 2
+led.plot(led2, 0)
+let dirc = 1
 basic.forever(function () {
-	
+    led.unplot(led2, 0)
+    led2 += dirc
+    led.plot(led2, 0)
+    basic.pause(500)
+    if (led2 >= 4) {
+        dirc = -1
+    } else if (led2 <= 0) {
+        dirc = 1
+    }
 })
